08ad71cf9cf6d99c30af282b5f35a75e088f22fb
ducnam3042005 <ducnam3042005@gmail.com>
2025-07-19 17:19:31 +0000

    update

--- pages/Calcul cout revient/jsobjects/JSObject1/JSObject1.js
+++ pages/Calcul cout revient/jsobjects/JSObject1/JSObject1.js
@@ -5,19 +5,19 @@ export default {
     const cr_ht = coeff * ctp;
     const tva = parseFloat(appsmith.store.tva);
     const cr_ttc = cr_ht * (1 + tva / 100);
-
-    storeValue("coeff", coeff.toFixed(2));
-    storeValue("cr_ht", cr_ht.toFixed(5));
-    storeValue("cr_ttc", cr_ttc.toFixed(5));
+		
+		storeValue("cr_ht", cr_ht.toFixed(5));
+		storeValue("coeff", coeff.toFixed(2));
+		storeValue("cr_ttc", cr_ttc.toFixed(5));
   },
 	
 	  onTvaChange: () => {
     const tva = parseFloat(Input_tva.text);
     const cr_ht = parseFloat(appsmith.store.cr_ht);
     const cr_ttc = cr_ht * (1 + tva / 100);
-
-    storeValue("tva", tva.toFixed(2));
-    storeValue("cr_ttc", cr_ttc.toFixed(5));
+		
+		storeValue("tva", tva.toFixed(2));
+		storeValue("cr_ttc", cr_ttc.toFixed(5));
   },
 	
 	  onCrHtChange: () => {
@@ -26,19 +26,19 @@ export default {
     const coeff = cr_ht / ctp;
     const tva = parseFloat(appsmith.store.tva);
     const cr_ttc = cr_ht * (1 + tva / 100);
-
-    storeValue("cr_ht", cr_ht.toFixed(5));
-    storeValue("coeff", coeff.toFixed(2));
-    storeValue("cr_ttc", cr_ttc.toFixed(5));
+		
+		storeValue("cr_ht", cr_ht.toFixed(5));
+		storeValue("coeff", coeff.toFixed(2));
+		storeValue("cr_ttc", cr_ttc.toFixed(5));
   },
 	
 	  onCrTtcChange: () => {
     const cr_ttc = parseFloat(Input_cout_revient_TTC.text);
     const cr_ht = parseFloat(appsmith.store.cr_ht);
     const tva = ((cr_ttc / cr_ht) - 1) * 100;
-
-    storeValue("cr_ttc", cr_ttc.toFixed(5));
-    storeValue("tva", tva.toFixed(2));
+		
+		storeValue("tva", tva.toFixed(2));
+		storeValue("cr_ttc", cr_ttc.toFixed(5));
   },
 	
 	resetValeurs: () => {
@@ -47,11 +47,11 @@ export default {
 		const tva = 5.5;
 		const cr_ht = ctp * coeff;
 		const cr_ttc = cr_ht * (1 + tva / 100);
-
-		storeValue("ctp_ht", ctp);
-		storeValue("coeff", coeff);
-		storeValue("tva", tva);
+		
 		storeValue("cr_ht", cr_ht.toFixed(5));
+		storeValue("coeff", coeff);
 		storeValue("cr_ttc", cr_ttc.toFixed(5));
+		storeValue("ctp_ht", ctp);
+		storeValue("tva", tva);
 	}
 }
