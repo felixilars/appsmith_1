0b81e15e7656d9116d4a22d2df377f46651fc4c4
ducnam3042005 <ducnam3042005@gmail.com>
2025-07-23 19:42:39 +0000

    update

--- pages/Calcul cout revient/jsobjects/JSObject1/JSObject1.js
+++ pages/Calcul cout revient/jsobjects/JSObject1/JSObject1.js
@@ -11,28 +11,28 @@ export default {
 		storeValue("cr_ttc", cr_ttc.toFixed(5));
   },
 	
-	  onTvaChange: () => {
-    const tva = parseFloat(Input_tva.text);
-    const cr_ht = parseFloat(appsmith.store.cr_ht);
-    const cr_ttc = cr_ht * (1 + tva / 100);
-		
+	onTvaChange: () => {
+		const tva = parseFloat(Input_tva.text);
+		const cr_ht = parseFloat(appsmith.store.cr_ht);
+		const cr_ttc = cr_ht * (1 + tva / 100);
+
 		storeValue("tva", tva.toFixed(2));
 		storeValue("cr_ttc", cr_ttc.toFixed(5));
-  },
+	},
 	
-	  onCrHtChange: () => {
-    const cr_ht = parseFloat(Input_cout_revient_HT.text);
-    const ctp = appsmith.store.ctp_ht;
-    const coeff = cr_ht / ctp;
-    const tva = parseFloat(appsmith.store.tva);
-    const cr_ttc = cr_ht * (1 + tva / 100);
-		
+	onCrHtChange: () => {
+		const cr_ht = parseFloat(Input_cout_revient_HT.text);
+		const ctp = appsmith.store.ctp_ht;
+		const coeff = cr_ht / ctp;
+		const tva = parseFloat(appsmith.store.tva);
+		const cr_ttc = cr_ht * (1 + tva / 100);
+
 		storeValue("cr_ht", cr_ht.toFixed(5));
 		storeValue("coeff", coeff.toFixed(2));
 		storeValue("cr_ttc", cr_ttc.toFixed(5));
-  },
+	},
 	
-	  onCrTtcChange: () => {
+	onCrTtcChange: () => {
     const cr_ttc = parseFloat(Input_cout_revient_TTC.text);
     const cr_ht = parseFloat(appsmith.store.cr_ht);
     const tva = ((cr_ttc / cr_ht) - 1) * 100;
