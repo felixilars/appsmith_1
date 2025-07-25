9b101d11d8b592ab2c0eafe3ed02bb94c9d153c4
ducnam3042005 <ducnam3042005@gmail.com>
2025-07-25 20:33:34 +0000

    update

--- pages/creer_matiere_premiere/jsobjects/JSObject1/JSObject1.js
+++ pages/creer_matiere_premiere/jsobjects/JSObject1/JSObject1.js
@@ -2,10 +2,10 @@ export default {
   check: async () => {
     if (
       !Input_nom.text || 
-      Select_categorie.selectedOptionLabel == null || 
-      Select_fournisseur.selectedOptionLabel == null || 
+      !Select_categorie.selectedOptionLabel || 
+      !Select_fournisseur.selectedOptionLabel || 
       !Input_prix.text || 
-      Select_unite.selectedOptionLabel == null || 
+      !Select_unite.selectedOptionLabel || 
       !Input_quantite_unite.text
     ) {
       showAlert("Veuillez remplir tous les champs obligatoires de la sous-recette.", "warning");
